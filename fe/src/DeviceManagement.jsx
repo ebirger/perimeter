8fc116c6ed4536abb992bc133a90b640c6495f53
Eyal Birger <eyal.birger@gmail.com>
2025-02-07 13:20:24 -0800

    sort & filter

--- fe/src/DeviceManagement.jsx
+++ fe/src/DeviceManagement.jsx
@@ -90,26 +90,50 @@ export default function DeviceManagement(props) {
         }
     );
   };
+  const dataSource = devices.filter(d=>d['status'] === props.state).map((device, idx) => (
+      {
+        key: idx + 1,
+        id: device.id,
+        name: device.hostname,
+        mac: device.mac_address,
+        ip: device.ip_address,
+        last_seen: device.last_seen,
+      }
+  ));
+
+  const getFilters = (c) => {
+      return {
+        filterSearch: true,
+        onFilter: (value, record) => record[c].startsWith(value),
+	filters: dataSource.map((d) => { return { text: d[c], value: d[c] }; }),
+      };
+  };
+
   const columns = [
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
+      ...getFilters('name'),
     },
     {
       title: 'MAC Address',
       dataIndex: 'mac',
       key: 'mac',
+      ...getFilters('mac'),
     },
     {
       title: 'IP Address',
       dataIndex: 'ip',
       key: 'ip',
+      ...getFilters('ip'),
     },
     {
       title: 'Last Seen',
       dataIndex: 'last_seen',
       key: 'last_seen',
+      sorter: (a, b) => new Date(a.last_seen) - new Date(b.last_seen),
+      render: (_, record) => dateFormat.format(new Date(record.last_seen)),
     },
     {
       title: 'Action',
@@ -141,16 +165,6 @@ export default function DeviceManagement(props) {
   }, []);
 
   console.log(devices);
-  const dataSource = devices.filter(d=>d['status'] === props.state).map((device, idx) => (
-      {
-        key: idx + 1,
-        id: device.id,
-        name: device.hostname,
-        mac: device.mac_address,
-        ip: device.ip_address,
-        last_seen: dateFormat.format(new Date(device.last_seen)),
-      }
-  ));
   return (loading ? <Spin /> :
     <Table tableLayout="auto" columns={columns} dataSource={dataSource} />
   );
