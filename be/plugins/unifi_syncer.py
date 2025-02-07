ce3e9778e76a97609bd4f0781c7eac57c2d12b5f
Eyal Birger <eyal.birger@gmail.com>
2025-02-07 11:41:24 -0800

    living in a pagination

--- be/plugins/unifi_syncer.py
+++ be/plugins/unifi_syncer.py
@@ -19,26 +19,48 @@ TIMEOUT = 30
 
 def unifi_get(rel_path=None, params=None):
     headers = {'X-API-KEY': os.environ.get('UNIFI_TOKEN')}
-    url = f'{UNIFI_BASE}/{UNIFI_SITE_BASE}'
-    if rel_path:
-        url = '/'.join([url, rel_path])
+    url = f'{UNIFI_BASE}/{rel_path}'
     resp = requests.get(url, headers=headers, timeout=TIMEOUT, params=params)
     resp.raise_for_status()
-    return resp.json()
+    ret = resp.json()
+    log.debug(json.dumps(ret, indent=2))
+    return ret
 
 
 def get_unifi_site_id():
     try:
-        resp = unifi_get()
+        resp = unifi_get(UNIFI_SITE_BASE)
         return resp['data'][0]['id']
     except:
         log.exception('failed to get Unifi site ID')
         return None
 
 
-def get_unifi_clients(site_id):
-    ret = unifi_get(f'{site_id}/clients', params={'limit': 200})
-    return ret['data']
+def get_unifi_client_names(site_id):
+    client_data = []
+    while True:
+        ret = unifi_get(f'{UNIFI_SITE_BASE}/{site_id}/clients',
+                        params={'limit': 200, 'offset': len(client_data)})
+        client_data += ret['data']
+        total_count = ret['totalCount']
+        n = len(client_data)
+        if n >= total_count:
+            break
+        log.info('got %s/%s', n, total_count)
+
+    return {mac: u['name'] for u in client_data if (mac := u.get('macAddress'))}
+
+
+def get_unifi_historical_client_names():
+    params = {
+        'onlyNonBlocked': 'true',
+        'includeUnifiDevices': 'true',
+        'withinHours': '0',
+    }
+    ret = unifi_get('proxy/network/v2/api/site/default/clients/history',
+                    params=params)
+    return {mac: name for u in ret
+            if ((mac := u.get('mac')) and (name := u.get('name')))}
 
 
 def get_perimeter_clients():
@@ -47,20 +69,16 @@ def get_perimeter_clients():
     return resp.json()
 
 
+
 def update_perimeter_client(c):
-    log.info('Updating device %s, mac %s, to name %s', c['id'],
-             c['mac_address'], c['hostname'])
     resp = requests.patch(f'{PERIMETER_BASE}/api/devices/{c["id"]}/',
                           json={'hostname': c['hostname']}, timeout=TIMEOUT)
 
 
 def enrich_perimeter_clients(pcs, ucs):
-    ucs = {mac: u for u in ucs if (mac := u.get('macAddress'))}
     log.debug(json.dumps(ucs, indent=2))
     for c in pcs:
-        if not (u := ucs.get(c['mac_address'])):
-            continue
-        if not (desired_name := u.get('name')):
+        if not (desired_name := ucs.get(c['mac_address'])):
             continue
         desired_name = desired_name.strip()
         if c['hostname'].strip() == desired_name:
@@ -84,8 +102,9 @@ if __name__ == '__main__':
 
     while True:
         try:
-            enrich_perimeter_clients(get_perimeter_clients(),
-                                     get_unifi_clients(site_id))
+            unifi_names = get_unifi_historical_client_names()
+            unifi_names.update(get_unifi_client_names(site_id))
+            enrich_perimeter_clients(get_perimeter_clients(), unifi_names)
         except:  # pylint: disable=bare-except
             log.exception('boom')
 
